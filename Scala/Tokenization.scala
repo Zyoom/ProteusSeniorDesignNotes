8541ea31cece669c4d6e03f3b95dddc25349e001
David Pedroza <107772031+Zyoom@users.noreply.github.com>
2024-02-06 10:18:47 -0800

    Update Tokenization.scala

--- Scala/Tokenization.scala
+++ Scala/Tokenization.scala
@@ -7,101 +7,71 @@ object Tokenization {
 
   // Define your token case classes or objects here
   case class Identifier(name: String) extends Token
-
   case class Number(value: Int) extends Token
-
   case object Plus extends Token
-
   case object Minus extends Token
-
   case object Multiply extends Token
-
   case object Divide extends Token
-
   case object OpenParenthesis extends Token
-
   case object CloseParenthesis extends Token
-
   case class FPPCodeBlock(code: String) extends Token
-
   case class ProteusCodeBlock(code: String) extends Token
 
+  // Tokenize function
   def tokenize(input: String): List[Token] = {
-    var tokens: List[Token] = List.empty
-    val currentToken: StringBuilder = new StringBuilder
-    var isFPPBlock = false
-    val fppBlockContent = new StringBuilder
-    var isProteusBlock = false
-    val proteusBlockContent = new StringBuilder
+    val operators: Map[Char, Token] = Map('+' -> Plus, '-' -> Minus, '*' -> Multiply, '/' -> Divide)
+    // Define special characters for FPP and Proteus blocks
+    val fppBlockStart = '['
+    val fppBlockEnd = "*]"
+    val proteusBlockStart = "(*)"
+    val proteusBlockEnd = ')'
 
-    def isOperator(c: Char): Boolean = Set('+', '-', '*', '/').contains(c)
-
-    def isParenthesis(c: Char): Boolean = Set('(', ')').contains(c)
-
-    for (char <- input) {
-      if (char.isWhitespace) {
-        // Ignore whitespace
-      }
-      else if (char == '[') {
+    // Recursive tokenization function
+    @scala.annotation.tailrec
+    def tokenizeRec(input: List[Char], acc: List[Token], currentToken: StringBuilder, fppBlockContent: StringBuilder, proteusBlockContent: StringBuilder, isFPPBlock: Boolean, isProteusBlock: Boolean): List[Token] = {
+      input match {
+        // Base case: if input is empty, return the accumulated tokens
+        case Nil =>
+          if (currentToken.nonEmpty) {
+            acc :+ (if (currentToken.forall(_.isDigit)) Number(currentToken.toString.toInt) else Identifier(currentToken.toString))
+          } else {
+            acc
+          }
         // Start of FPP code block
-        isFPPBlock = true
-        currentToken.append(char)
-      }
-      else if (char == '*' && isFPPBlock) {
-        // End of FPP code block
-        isFPPBlock = false
-        currentToken.append(char)
-        tokens = tokens :+ FPPCodeBlock(fppBlockContent.toString)
-        fppBlockContent.clear()
-      }
-      else if (char == '(' && currentToken.endsWith("(*)")) {
-        // StaRt of Proteus code block
-        isProteusBlock = true
-        currentToken.append(char)
-      }
-      else if (char == ')' && isProteusBlock) {
-        // Ed of Proteus code block
-        isProteusBlock = false
-        currentToken.append(char)
-        tokens = tokens :+ ProteusCodeBlock(proteusBlockContent.toString) // new instance made, & convert to string
-        proteusBlockContent.clear()
-      }
-      else if (isFPPBlock) {
+        case '[' :: tail =>
+          if (isFPPBlock) tokenizeRec(tail, acc :+ FPPCodeBlock(fppBlockContent.toString), currentToken, new StringBuilder, proteusBlockContent, isFPPBlock = false, isProteusBlock)
+          else tokenizeRec(tail, acc :+ FPPCodeBlock(""), currentToken.append(fppBlockStart), fppBlockContent, proteusBlockContent, isFPPBlock = true, isProteusBlock)
+        // Start of Proteus code block
+        case '(' :: '*' :: tail if currentToken.endsWith(fppBlockEnd) =>
+          tokenizeRec(tail, acc, currentToken.append(proteusBlockStart), fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock = true)
+        // End of Proteus code block
+        case '*' :: ')' :: tail if isProteusBlock =>
+          if (currentToken.endsWith(proteusBlockStart)) tokenizeRec(tail, acc :+ ProteusCodeBlock(proteusBlockContent.toString), currentToken, fppBlockContent, new StringBuilder, isFPPBlock, isProteusBlock = false)
+          else tokenizeRec(tail, acc, currentToken.append(proteusBlockEnd), fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock)
+        // Ignore whitespace
+        case c :: tail if c.isWhitespace =>
+          tokenizeRec(tail, acc, currentToken, fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock)
         // Inside FPP code block
-        fppBlockContent.append(char)
-      }
-      else if (isProteusBlock) {
-        // Iside Proteus code block
-        proteusBlockContent.append(char)
-      }
-      else if (char.isDigit) {
-        // Handle numbers
-        currentToken.append(char)
-      }
-      else if (char.isLetter) {
-        // Handle identifiers
-        currentToken.append(char)
-      }
-      else if (isOperator(char)) {
-        // Hadle operators
-        tokens = tokens :+ (char match {
-          case '+' => Plus
-          case '-' => Minus
-          case '*' => Multiply
-          case '/' => Divide
-        })
-      }
-      else {
-        // Handle other characters if needed
+        case c :: tail if isFPPBlock =>
+          tokenizeRec(tail, acc, currentToken, fppBlockContent.append(c), proteusBlockContent, isFPPBlock, isProteusBlock)
+        // Inside Proteus code block
+        case c :: tail if isProteusBlock =>
+          tokenizeRec(tail, acc, currentToken, fppBlockContent, proteusBlockContent.append(c), isFPPBlock, isProteusBlock)
+        // Handle numbers or identifiers
+        case c :: tail if c.isDigit || c.isLetter =>
+          tokenizeRec(tail, acc, currentToken.append(c), fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock)
+        // Handle operators
+        case c :: tail if operators.contains(c) =>
+          val token = operators(c)
+          tokenizeRec(tail, acc :+ token, currentToken, fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock)
+        // Other characters
+        case _ :: tail =>
+          tokenizeRec(tail, acc, currentToken, fppBlockContent, proteusBlockContent, isFPPBlock, isProteusBlock)
       }
     }
 
-    // Handle the last token, if any
-    if (currentToken.nonEmpty) {
-      tokens = tokens :+ (if (currentToken.forall(_.isDigit)) Number(currentToken.toString.toInt) else Identifier(currentToken.toString))
-    }
-
-    tokens
+    // Call the recursive function with initial values
+    tokenizeRec(input.toList, List.empty, new StringBuilder, new StringBuilder, new StringBuilder, isFPPBlock = false, isProteusBlock = false)
   }
 }
 
